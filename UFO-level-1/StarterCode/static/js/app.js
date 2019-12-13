af961ab450e71851b65d62890a3f56bdabec9131
BDomogalla <BDDomogalla@gmail.com>
2019-12-13 15:46:11 -0800

    Level 2 working

--- UFO-level-1/StarterCode/static/js/app.js
+++ UFO-level-1/StarterCode/static/js/app.js
@@ -11,13 +11,13 @@ var table = d3.select("#ufo-table")
 // Make sure you have a column for date/time, city, state, country, shape, and comment at the very least.
 
 // Add all data- page will display full table before a specific date is entered as input
-data.forEach((ufo) => {
-    var row = table.append("tr");
-    Object.entries(ufo).forEach(([key, value]) => {
-      var cell = row.append("td");
-      cell.text(value);
-    });
-  });
+// data.forEach((ufo) => {
+//     var row = table.append("tr");
+//     Object.entries(ufo).forEach(([key, value]) => {
+//       var cell = row.append("td");
+//       cell.text(value);
+//     });
+//   });
   
 // Use a date form in your HTML document and write JavaScript code that will listen for events 
 // and search through the date/time column to find rows that match user input.
@@ -33,7 +33,7 @@ button.on("click", function() {
     console.log(tableData)
 
     // Clear data from table
-    table.html("");
+    // table.html("");
     
     // Append table data based on input date
     var dateData = tableData.filter(dTime => dTime.datetime === inputValue);
--- UFO-level-2/StarterCode/static/js/app.js
+++ UFO-level-2/StarterCode/static/js/app.js
@@ -11,13 +11,13 @@ var table = d3.select("#ufo-table")
 // Make sure you have a column for date/time, city, state, country, shape, and comment at the very least.
 
 // Add all data- page will display full table before a specific date is entered as input
-data.forEach((ufo) => {
-    var row = table.append("tr");
-    Object.entries(ufo).forEach(([key, value]) => {
-      var cell = row.append("td");
-      cell.text(value);
-    });
-  });
+// data.forEach((ufo) => {
+//     var row = table.append("tr");
+//     Object.entries(ufo).forEach(([key, value]) => {
+//       var cell = row.append("td");
+//       cell.text(value);
+//     });
+//   });
   
 // Use a date form in your HTML document and write JavaScript code that will listen for events 
 // and search through the date/time column to find rows that match user input.
@@ -25,7 +25,7 @@ data.forEach((ufo) => {
 var button = d3.select("#filter-btn");
 
 button.on("click", function() {
-    var inputElement = d3.select("#datetime");
+    var inputElement = d3.select("#category");
     var inputValue = inputElement.property("value");
 
     // Logging input value and table data in console
@@ -33,12 +33,55 @@ button.on("click", function() {
     console.log(tableData)
 
     // Clear data from table
-    table.html("");
+    // table.html("");
     
+
     // Append table data based on input date
     var dateData = tableData.filter(dTime => dTime.datetime === inputValue);
     console.log(dateData);
     dateData.forEach((ufo) => {
+        var row = table.append("tr");
+        Object.entries(ufo).forEach(([key, value]) => {
+          var cell = row.append("td");
+          cell.text(value);
+        });
+      });
+     // Append table data based on input city
+     var cityData = tableData.filter(cities => cities.city === inputValue);
+     console.log(cityData);
+     cityData.forEach((ufo) => {
+         var row = table.append("tr");
+         Object.entries(ufo).forEach(([key, value]) => {
+           var cell = row.append("td");
+           cell.text(value);
+         });
+       });
+
+       // Append table data based on input state
+    var stateData = tableData.filter(states => states.state === inputValue);
+    console.log(stateData);
+    stateData.forEach((ufo) => {
+        var row = table.append("tr");
+        Object.entries(ufo).forEach(([key, value]) => {
+          var cell = row.append("td");
+          cell.text(value);
+        });
+      });
+
+     // Append table data based on input country
+     var countryData = tableData.filter(countries => countries.country === inputValue);
+     console.log(countryData);
+     countryData.forEach((ufo) => {
+         var row = table.append("tr");
+         Object.entries(ufo).forEach(([key, value]) => {
+           var cell = row.append("td");
+           cell.text(value);
+         });
+       });
+       // Append table data based on input shape
+    var shapeData = tableData.filter(shapes => shapes.shape === inputValue);
+    console.log(shapeData);
+    shapeData.forEach((ufo) => {
         var row = table.append("tr");
         Object.entries(ufo).forEach(([key, value]) => {
           var cell = row.append("td");
